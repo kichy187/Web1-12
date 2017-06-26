cb15e0127657a69ec2f0a53c25198321a2666e8f
kichy187 <ttynzm1998@gmail.com>
2017-06-26 17:34:39 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,22 +1,11 @@
 document.getElementById('form').onsubmit = function(){
   window.alert(document.getElementById('form').word.value);
 };
+
 var message;
 var messaget;
  for(var i = 1; i < 100;i++){
    var li= document.createElement('li');
    li.textContent = i + '回目';
    document.getElementById('list').appendChild(li);
-   var keisan = i%3;
-   if(keisan = 1)
-   {
-     message = Fizz;
-   }
-   keisan = i%5;
-   if(keisan = 1)
-   {
-     messaget = Buzz;
-   }
-   document.getElementById('re').textContent = message;
-   document.getElementById('rere').textContent = messaget;
  }
