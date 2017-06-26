31d51dbeaffea3db798fed1f33981f5a3075da26
kichy187 <ttynzm1998@gmail.com>
2017-06-26 17:58:23 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,10 +2,23 @@ document.getElementById('form').onsubmit = function(){
   window.alert(document.getElementById('form').word.value);
 };
 
-var message;
-var messaget;
  for(var i = 1; i < 101; i++){
    var li = document.createElement('li');
+   var message = document.createElement('message');
+   var messaget = document.createElement('messaget');
+   
    li.textContent = i + '回目';
    document.getElementById('list').appendChild(li);
+   var keisan = i%3;
+   if(keisan = 1)
+   {
+     message.textContent = 'Fizz';
+     document.getElementById('re').appendChild(message);
+   }
+   var keisann = i%5;
+   if(keisann = 1)
+   {
+     messaget.textContent = 'Buzz';
+     document.getElementById('rere').appendChild(messaget);
+   }
  }
