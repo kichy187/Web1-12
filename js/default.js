647caa7352354b90101951dfd7721cc8cb9f5e7d
kichy187 <ttynzm1998@gmail.com>
2017-06-26 17:35:10 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -8,4 +8,17 @@ var messaget;
    var li= document.createElement('li');
    li.textContent = i + '回目';
    document.getElementById('list').appendChild(li);
+   
+   var keisan = i%3;
+   if(keisan = 1)
+   {
+     message = Fizz;
+   }
+   keisan = i%5;
+   if(keisan = 1)
+   {
+     messaget = Buzz;
+   }
+   document.getElementById('re').textContent = message;
+   document.getElementById('rere').textContent = messaget;
  }
