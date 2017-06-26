f9b69024dbbd6034ab3c2de15dd3fbd5dc3ddf5a
kichy187 <ttynzm1998@gmail.com>
2017-06-26 17:49:11 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -4,8 +4,21 @@ document.getElementById('form').onsubmit = function(){
 
 var message;
 var messaget;
- for(var i = 1; i < 100; i++){
+ for(var i = 1; i <= 100; i++){
    var li = document.createElement('li');
    li.textContent = i + '回目';
    document.getElementById('list').appendChild(li);
+   
+   var keisan = i%3;
+   if(keisan === 1)
+   {
+     message = Fizz;
+     document.getElementById('re').textContent = message;
+   }
+   keisan = i%5;
+   if(keisan === 1)
+   {
+     messaget = Buzz;
+     document.getElementById('rere').textContent = messaget;
+   }
  }
